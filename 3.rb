5335e76ee9768172f4e439bafbbad516af84d950
Heahmund <tima260399@mail.ru>
2022-12-07 12:08:09 +0200

    Lesson12. Issue #8355

--- 3.rb
+++ 3.rb
@@ -1,4 +1,10 @@
 print "сколько вы прожили секунд? "
-Num = (gets.chomp.to_i)/31556926
-puts "#{Num.to_i} лет? А по вам и не скажешь!"
+var1 = (gets.chomp.to_i)
+var2 = Time.now
+birth = var2 - var1
+goal = Time.at(var2).year - Time.at(birth).year
+if var2.yday <= birth.yday
+goal += 1
+end
+puts "Вы родились #{Time.at(birth)}? получается вам #{goal}! А по вам и не скажешь!"
 gets()
