a3e73b3212e42dc8e8aac62cef7c62ee9474639a
Heahmund <tima260399@mail.ru>
2022-12-07 10:16:19 +0200

    Lesson1.  Issue #8355

--- 4.rb
+++ 4.rb
@@ -1,19 +1,15 @@
-file_path = "/home/musaev/tmp/Ruby4Beginners/agile/input.txt"
-sum=""
-
-output=[]
-file_i = File.new(file_path, "r:UTF-8")
-file_o = File.new("./output.txt", "w:UTF-8") 
+output = []
+file_i = File.open("input.txt",  "r:UTF-8")
 lines = file_i.readlines
 file_i.close
 lines.each do |n|
-    n.each_char{|d| sum+=d}
-    r=Math.sqrt(sum.to_f)
-    puts"#{r}"
-    output.push(r)
-    r=0
-    sum=""
-  end
-output.reverse.each{|i|file_o.print("#{i}\n")}
+  n.gsub!(' ', '')
+  n.gsub!("\n", '')
+  n = n.to_f
+  r = Math.sqrt(n)
+  output << r
+end
+file_o = File.open("./output.txt", "w:UTF-8") 
+output[-1..0].each{ |i| file_o.puts i }
 file_o.close
 gets()
